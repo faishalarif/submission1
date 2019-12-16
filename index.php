bde1a6a2bfdced864a45ab809e7b633fae835f02
faishalarif <55336857+faishalarif@users.noreply.github.com>
2019-12-16 23:16:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,12 +26,12 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "faishalarif.azurewebsites.net,1433";
+    $host = "faishalarif.azurewebsites.net";
     $user = "faishal";
     $pass = "chale@2pm";
     $db = "faishalarif";
     try {
-    $conn = new PDO("sqlsrv:server = tcp:faishalarif.database.windows.net,1433; Database = faishalarif", "faishal", "chale@2pm");
+    $conn = new PDO("sqlsrv:server = tcp:faishalarif.database.windows.net; Database = faishalarif", "faishal", "chale@2pm");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -39,7 +39,7 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 $connectionInfo = array("UID" => "faishal", "pwd" => "chale@2pm", "Database" => "faishalarif", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:faishalarif.database.windows.net,1433";
+$serverName = "tcp:faishalarif.database.windows.net";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
     }
